9313e49bf2b90a6388dcedfe4697f906a889313d
Alex Stolzoff <alexstolznut@hotmail.com>
2017-08-25 02:00:18 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,17 +1,17 @@
 var array=[];
 
-// function forLoop(array){
-//
-//   for(let i = 0; i<25; i = i++){
-//     var t= i+1;
-//     if (i==0){
-//       array[i]="I am 1 strange loop.";
-//     } else{
-//     array[i]=`I am ${t} strange loops.`;
-//     }
-//   }
-//   return array;
-// }
+function forLoop(array){
+
+  for(let i = 0; i<25; i = i++){
+    var t= i+1;
+    if (i==0){
+      array[i]="I am 1 strange loop.";
+    } else{
+    array[i]=`I am ${t} strange loops.`;
+    }
+  }
+  return array;
+}
 //
 // function whileLoop(n){
 //   while(n>0){
