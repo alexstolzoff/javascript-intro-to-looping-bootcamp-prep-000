4ba3ad857841b03022a9df3f67d253e58aaa0083
Alex Stolzoff <alexstolznut@hotmail.com>
2017-08-25 02:05:00 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -7,7 +7,7 @@ function forLoop(array){
     if (i==0){
       array.push("I am 1 strange loop.");
     } else{
-    array.push(`I am ${t} strange loops.`);
+    array.push(`I am ${i} strange loops.`);
     }
   }
   return array;
